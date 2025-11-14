ab85b34e40eb5d382e85fdd8dce34765010c0de8
gaspard-psq <gaspard.pasquier26100@gmail.com>
2025-11-14 10:24:23 +0100

    connexion6

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
     <input type="submit" value="Connexion" />
 
         <footer>
-            <p>Copyright © Mathieu Mangeot</p>
+            <p>Copyright © Gaspard Pasquier</p>
         </footer>
 </body>
 </html>
