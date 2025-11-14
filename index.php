dc47693b6220ac9a4411c9c2871e887c18380e1b
gaspard-psq <gaspard.pasquier26100@gmail.com>
2025-11-14 10:29:34 +0100

    include

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr" prefix="og: https://ogp.me/ns#">
-<head>
-  <link rel="stylesheet" href="style.css">
-  <meta charset="utf-8" />
-  <meta name="author" content="Gaspard Pasquier" />
-  <meta name="keywords" content="document html5 mmi" />
-  <meta name="description" content="Document minimal html5 en DUT MMI" />
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta property="og:type" content="website" />
-  <meta property="og:url" content="http://mmi.univ-smb.fr/~mmang/" />
-  <meta property="og:image" content="http://mmi.univ-smb.fr/~mmang/image.png" />
-  <meta property="og:locale" content="fr_FR" />
-  <meta property="og:description" content="Document minimal html5 en DUT MMI" />
-  <meta property="og:title" content="Document minimal en MMI" />
-  <meta property="og:site_name" content="Document minimal en MMI" />
-  <title>Document minimal en MMI</title>
-</head>
+include('entete.php')
 
-<body>
     <header>
             <h1>Page de connexion</h1>
         <nav>
@@ -38,8 +20,4 @@
     </div>
     <input type="submit" value="Connexion" />
 
-        <footer>
-            <p>Copyright © Gaspard Pasquier</p>
-        </footer>
-</body>
-</html>
+include('pieddepage.php')        
---
+++ pieddepage.php
@@ -0,0 +1,5 @@
+<footer>
+            <p>Copyright © Gaspard Pasquier</p>
+        </footer>
+</body>
+</html>
